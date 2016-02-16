7cb920476600abdce43767085229570ea2d5f650
Moisés Cachay Tello <mcachay@phantasia.pe>
2016-02-15 19:08:21 -0500

    updated Mon Feb 15 19:08:21 PET 2016

--- tweet.sh
+++ tweet.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-cd $(dirname $0) && git add . && git commit -m "updated `date`" && git push
+
+if [ "$1" == "pre" ]; then
+    cd $(dirname $0) && git pull
+elif [ "$1" == "post" ]; then
+    cd $(dirname $0) && git add . && git commit -m "updated `date`" && git push
+else 
+    echo "Unsupported argument."
+fi
+
